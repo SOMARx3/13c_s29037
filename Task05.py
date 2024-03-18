ac32913d41c76075f7bb20575da3ed23db1ce80a
Maksym <s29037@pjwstk.edu.pl>
2024-03-18 13:28:38 +0100

    Task 5 finished

---
+++ Task05.py
@@ -0,0 +1,8 @@
+class someClass:
+    def func(start1, end):
+        if (end < start1):
+            raise Exception("end smaller than start")
+        else:
+            return [x * x for x in range(start1, end)]
+
+print(someClass.func(11, 1))
